70e66e9975c31ba916e8b173780a51026fcd79f8
IvanDrv <ivander05@mail.ru>
2025-11-08 21:19:24 +0300

    Controller with morse code endpoints

---
+++ converter/src/main/java/com/service/converter/controller/ConverterController.java
@@ -0,0 +1,34 @@
+package com.service.converter.controller;
+
+import com.service.converter.model.Dispatcher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/convert")
+public class ConverterController {
+    @Autowired
+    private Dispatcher model;
+
+    @GetMapping("/test")
+    public String test(){
+        return "Тестовый GET работает!";
+    }
+
+    @PostMapping("/test")
+    public String test(@RequestBody String t){
+        return "String input: "+t;
+    }
+
+    @PostMapping("/morseEncode")
+    public String morseEncode(@RequestBody String txt){
+        return model.convertToCode(txt);
+    }
+
+    @PostMapping("/morseDecode/{language}")
+    public String morseEncode(@RequestBody String txt, @PathVariable String language){
+        return model.convertToText(txt, language);
+    }
+
+}
